63cfd53481d964b55363cf985909b12d31207bdf
ahsisnwk <118455540+ahsisnwk@users.noreply.github.com>
2023-03-20 23:29:11 +0800

    更新 zxcs.js

--- remote/zxcs.js
+++ remote/zxcs.js
@@ -4,13 +4,11 @@ switch(true)
 {
 case url.includes("zxcs.me/post"):
 
-body=body.replace(/<\/body>/g,'<script>$(".yinyong,#vote,.clear,.log_a,#footer,#lujing,.postinfo,.ajax_comment").remove();</script></body>');
-break;
+$(".yinyong,#vote,.clear,.log_a,#footer,#lujing,.postinfo,.ajax_comment").remove();
 
 
 
 case url.includes("zxcs.me/download"):
 
-body=body.replace(/<\/body>/g,'<script>$(".diybanner,.banner,.panel:last").remove(); </script></body>');
-break;
+$(".diybanner,.banner,.panel:last").remove();
 }
