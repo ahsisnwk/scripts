85c29a7384328ed7d91aaf2c824d965b5f243bf2
ahsisnwk <118455540+ahsisnwk@users.noreply.github.com>
2023-05-25 20:23:30 +0800

    Delete remote directory

--- remote/bdys.js
+++
@@ -1,5 +0,0 @@
-var body=$response.body;
-
-body=body.replace(/<div class="col-12[\s\S]*?<\/center>/g,'');
-
-$done(body);
--- remote/yazhouse8.js
+++
@@ -1,5 +0,0 @@
-var body=$response.body;
-
-body=body.replace(/<div class="banners"[\s\S]*?<\/div>/g,'');
-
-$done(body);
